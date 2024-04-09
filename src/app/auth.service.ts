88dfdc232798d54fc5947809dfa427ccd7e6d1b7
Anouar-91 <a.berrouane@hotmail.com>
2024-04-09 14:06:38 +0200

    eslint

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,30 +1,30 @@
 import { Injectable, inject, signal } from "@angular/core";
-import {Auth, User, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile, user} from "@angular/fire/auth";
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile, user } from "@angular/fire/auth";
 import { Observable, from } from "rxjs";
 
 
 interface UserInterface {
-    email: string|null;
-    username:string|null;
-  }
+    email: string | null;
+    username: string | null;
+}
 @Injectable({
     providedIn: 'root'
 })
 
 export class AuthService {
-    
+
     fireBaseAuth = inject(Auth)
     user$ = user(this.fireBaseAuth);
     currentUserSig = signal<UserInterface | null | undefined>(undefined);
 
-    register(email:string,username:string,password:string):Observable<void>{
-        const promise = createUserWithEmailAndPassword(this.fireBaseAuth,email, password ).then(
-            response => updateProfile(response.user, {displayName: username})
+    register(email: string, username: string, password: string): Observable<void> {
+        const promise = createUserWithEmailAndPassword(this.fireBaseAuth, email, password).then(
+            response => updateProfile(response.user, { displayName: username })
         );
         return from(promise)
     }
 
-    login(email:string,password:string):Observable<void>{
+    login(email: string, password: string): Observable<void> {
         const promise = signInWithEmailAndPassword(
             this.fireBaseAuth,
             email,
@@ -35,7 +35,7 @@ export class AuthService {
         return from(promise);
     }
 
-    logout():Observable<void>{
+    logout(): Observable<void> {
         const promise = signOut(this.fireBaseAuth);
         return from(promise);
     }
